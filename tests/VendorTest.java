5c29b9c0232df3a81edbf75fbe346616c7cb3724
Minash23 <minas27188@gmail.com>
2024-11-07 09:59:27 -0600

    Test to validate that you can add money to a vendor

--- tests/VendorTest.java
+++ tests/VendorTest.java
@@ -1,12 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VendorTest {
 
-    @Test
-    void addition() {
-        assertEquals(2, 1 + 1);
+    static Vending vendor;
+
+    @BeforeAll
+    public static void setUp(){
+        vendor = new Vending(10, 10);
     }
+
+    //Test to check if you can add money to vendor
+    @Test
+    void canAddMoney(){
+        double initialBalance = vendor.getBalance();
+        vendor.addMoney(5.0);
+        assertEquals(initialBalance + 5.0, vendor.getBalance(), "Balance should increase by the amount added");
+    }
+
 
 }
